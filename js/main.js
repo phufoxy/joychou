e2984a30a35ff81fe3b084be2ac0531a72138b83
vanphudhsp2015 <vanphudhsp2015@gmail.com>
2019-02-23 22:44:23 +0700

    upload business

--- js/main.js
+++ js/main.js
@@ -38,6 +38,10 @@ jQuery(document).ready(function ($) {
             var $this = $(this);
             $this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body');
         });
+        $('.js-clone-lang').each(function () {
+            var $this = $(this);
+            $this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-lang');
+        });
         setTimeout(function () {
             var counter = 0;
             $('.site-mobile-menu .has-children').each(function () {
